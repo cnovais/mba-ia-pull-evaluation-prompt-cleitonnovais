9c56813233339854964f9d1616e57b036bc41794
Software Architeture 2 <software2architeture@fiap.com.br>
2026-06-06 00:42:09 -0300

    feito: 1. Pull do Prompt inicial do LangSmith

--- src/pull_prompts.py
+++ src/pull_prompts.py
@@ -16,16 +16,33 @@ from dotenv import load_dotenv
 from langchain import hub
 from utils import save_yaml, check_env_vars, print_section_header
 
+#minhas alterações
+from langsmith import Client
+
 load_dotenv()
 
+client = Client()
+
+def save_prompt_locally(prompt):
+    Path("prompts").mkdir(exist_ok=True)
+    system_template = prompt.messages[0].prompt.template
+    human_template = prompt.messages[1].prompt.template
+
+    with open("prompts/bug_to_user_story_v1.yml", "w", encoding="utf-8") as f:
+        f.write(system_template + "\n\n" + human_template)
+
+    print("Prompt salvo localmente")
 
 def pull_prompts_from_langsmith():
-    ...
+    prompt = client.pull_prompt("leonanluppi/bug_to_user_story_v1")
+    print(f"Prompt '{prompt.name}' puxado do LangSmith Hub")
+    return prompt
 
 
 def main():
     """Função principal"""
-    ...
+    prompt = pull_prompts_from_langsmith()
+    save_prompt_locally(prompt)
 
 
 if __name__ == "__main__":
